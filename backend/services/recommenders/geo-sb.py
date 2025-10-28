fcc00ee40cf53117e7f3d65d0692931df34ff817
akira-zip <jrgtf.space@gmail.com>
2025-10-28 05:58:16 -0300

    Mod - [geo-sp.py]
    Adicionei o limit, pois se existisse muitos usuários, essa ação poderia sobrecarregar o backend.

--- backend/services/recommenders/geo-sb.py
+++ backend/services/recommenders/geo-sb.py
@@ -3,14 +3,14 @@ import supabase as sb
 url = "https://mflpegvqdnqdfvbvfdos.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mbHBlZ3ZxZG5xZGZ2YnZmZG9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjAwNDQ3ODEsImV4cCI6MjA3NTYyMDc4MX0.DLcfAc8u1dA91oISnOEMTIq1GxQZ7AAUXWiWmlf0Uo4"
 
-def recommendGeo(user):
+def recommendGeo(user, limit=10):
   try:
     client = sb.create_client(url, key)
   except Exception as e:
     print(e)
   
   try:
-    raw = client.table("musics").select("title, artist_id(name)").execute()
+    raw = client.table("musics").select("title, artist_id(name)").limit(limit).execute()
   except Exception as e:
     print(e)
   
