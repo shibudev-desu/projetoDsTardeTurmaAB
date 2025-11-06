30a6e6008225bb75eba91b3cd2a09336b52dd15b
João Paulo <joao.silva3371@etec.sp.gov.br>
2025-11-06 18:14:53 -0300

    Arrumando o router import no main.py e test_music.py.

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,18 @@
 from fastapi import FastAPI
-from app.routers import users, music, artists, popular
+from app.routers.users import router as users_router
+from app.routers.music import router as music_router
+from app.routers.artists import router as artists_router
+from app.routers.styles import router as styles_router
+from services.popular import router as popular_router
 from app.db.supabase_client import get_supabase, SUPABASE_KEY, SUPABASE_URL
 
 app = FastAPI(title="Backend React Native API")
 
 # incluir rotas
-app.include_router(users, prefix="/api/users", tags=["users"])
-app.include_router(artists, prefix="/api/artists", tags=["artists"])
-app.include_router(music, prefix="/api/music", tags=["music"])
-app.include_router(popular, prefix="/api/popular", tags=["popular"])
+app.include_router(users_router, prefix="/api/users", tags=["users"])
+app.include_router(artists_router, prefix="/api/artists", tags=["artists"])
+app.include_router(music_router, prefix="/api/music", tags=["music"])
+app.include_router(popular_router, prefix="/api/popular", tags=["popular"])
 
 @app.get("/")
 def root():
