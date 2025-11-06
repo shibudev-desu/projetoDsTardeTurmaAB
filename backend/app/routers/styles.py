18c0ee39c49021a856bb0e418c10581e4eeada85
João Paulo <joao.silva3371@etec.sp.gov.br>
2025-11-06 17:59:39 -0300

    Adicionando Router

--- backend/app/routers/styles.py
+++ backend/app/routers/styles.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter
 from app.models import Styles
 from app.db.supabase_client import get_supabase
 
+router = APIRouter()
 
 @router.get("/selectstyles")
 def get_styles():
     supabase = get_supabase()
     response = supabase.table("styles").select("*").execute()
-    return response.data  
+    return response.data
 
