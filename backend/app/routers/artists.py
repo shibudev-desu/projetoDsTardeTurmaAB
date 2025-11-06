d8e3a9c9e747055c9ea545ca7d5e9227fb275c90
João Paulo <joao.silva3371@etec.sp.gov.br>
2025-11-06 17:15:16 -0300

    Atualizando pra ser integrado

--- backend/app/routers/artists.py
+++ backend/app/routers/artists.py
@@ -1,12 +1,5 @@
 """
-
-Este módulo define rotas para operações CRUD de artistas usando FastAPI.
-
-Variáveis:
-- fake_db: Lista simulando um banco de dados de usuários, cada artista é um dicionário com 'id' e 'name'.
-
-Nota: Este código utiliza um banco de dados em memória apenas para fins de demonstração.
-
+CRUD de artistas usando FastAPI e banco fake em memória.
 """
 
 from fastapi import APIRouter
@@ -15,32 +8,66 @@ from app.db.fake_db import fake_db
 
 router = APIRouter()
 
+# GET - Buscar todos artistas
 @router.get("/")
-def get_users():
-    return fake_db.artist
+def get_artists():
+    return fake_db['artists']
 
+# GET - Buscar artista por ID
 @router.get("/{artist_id}")
 def get_artist(artist_id: int):
-    artist = next((artist for artist in fake_db if artist["id"] == artist_id), None)
-    if artist:
-        return artist
+    artist = next((artist for artist in fake_db["artists"] if artist["id"] == artist_id), None)
 
+    if not artist:
+        return {"error": "Artist not found"}
+
+    return artist
+
+# POST - Criar artista
 @router.post("/")
 def create_artist(artist: Artist):
-    new_artist = {"id": len(fake_db) + 1, "name": artist.name}
-    fake_db.artists.append(new_artist)
+    new_artist = {
+        "id": len(fake_db['artists']) + 1,
+        "name": artist.name,
+        "email": artist.email,
+        "password": artist.password,
+        "bio": artist.bio,
+        "musics": artist.musics,
+    }
+
+    fake_db['artists'].append(new_artist)
+
     return new_artist
 
+# PUT - Atualizar artista
 @router.put("/{artist_id}")
 def update_artist(artist_id: int, artist: Artist):
-    if artist_id > len(fake_db):
+
+    index = next((i for i, a in enumerate(fake_db['artists']) if a["id"] == artist_id), None)
+
+    if index is None:
         return {"error": "Artist not found"}
-    fake_db.artists[artist_id - 1] = {"id": artist_id, "name": artist.name}
+
+    fake_db['artists'][index] = {
+        "id": artist_id,
+        "name": artist.name,
+        "email": artist.email,
+        "password": artist.password,
+        "bio": artist.bio,
+        "musics": artist.musics,
+    }
+
     return {"message": "Artist updated"}
 
+# DELETE - Deletar artista
 @router.delete("/{artist_id}")
 def delete_artist(artist_id: int):
-    if artist_id > len(fake_db):
+
+    index = next((i for i, a in enumerate(fake_db['artists']) if a["id"] == artist_id), None)
+
+    if index is None:
         return {"error": "Artist not found"}
-    del fake_db.artists[artist_id - 1]
+
+    del fake_db['artists'][index]
+
     return {"message": "Artist deleted"}
