16a80a3e5d03a53583af3333b8d24a1b24734882
João Paulo <joao.silva3371@etec.sp.gov.br>
2025-11-06 17:15:30 -0300

    Incluindi rotas

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
-from app.routers import users, music
-from services import popular
+from app.routers import users, music, artists, popular
 from app.db.supabase_client import get_supabase, SUPABASE_KEY, SUPABASE_URL
+
 app = FastAPI(title="Backend React Native API")
 
 # incluir rotas
 app.include_router(users.router, prefix="/api/users", tags=["users"])
+app.include_router(artists.router, prefix="/api/artists", tags=["artists"])
 app.include_router(music.router, prefix="/api/music", tags=["music"])
-app.include_router(popular.router, prefix="/api", tags=["popular"])
+app.include_router(popular.router, prefix="/api/popular", tags=["popular"])
 
 @app.get("/")
 def root():
