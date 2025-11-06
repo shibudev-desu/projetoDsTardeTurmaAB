4628971bad1f725f56d8015a6c5bc34cead37056
João Paulo <joao.silva3371@etec.sp.gov.br>
2025-11-06 16:39:17 -0300

    ajustando tracer

--- tracer/src/index.ts
+++ tracer/src/index.ts
@@ -1,6 +1,20 @@
-import { ENVIRONMENT } from "./environment/env"
 import express from 'express'
+import mongoose from 'mongoose'
+import { ENVIRONMENT } from "./environment/env.js"
+import logsRouter from './routes/logs.js'
+
 const app = express()
+
+// Middleware to parse JSON
+app.use(express.json())
+
+// Connect to MongoDB
+mongoose.connect(ENVIRONMENT.DATABASE_URI as string)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('MongoDB connection error:', err))
+
+// Use routes
+app.use('/logs', logsRouter)
 
 app.listen(ENVIRONMENT.PORT, () => {
     console.log("SERVER LISTENING ON PORT " + ENVIRONMENT.PORT)
